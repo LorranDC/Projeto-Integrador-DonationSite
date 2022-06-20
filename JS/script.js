828ecc82a602f49b0bcbbb55179ee45e3f10c031
Lorran Dias Carmácio <lorrandias150@gmail.com>
2022-06-19 22:01:18 -0300

    Atualização e refinamento feito por Lorran em andamento

--- JS/script.js
+++ JS/script.js
@@ -1,7 +1,29 @@
-function showForm() {
-    document.getElementById('formElement').style.display = 'block';
+
+function selecionarCidade() {
+const select = document.querySelector('#city-choice');
+
+console.log(select);
+
+const indice = select.selectedIndex
+const value = select.value
+const text = select.options[indice].text
+
+localStorage.texto = text
+
+return text;
 }
-        
-    
+
+
+
+
+function imprimirCidade() {
+
+var tag = document.createElement("p");
+var newText = document.createTextNode(localStorage.texto);
+tag.appendChild(newText);
+var element = document.getElementById("newCity");
+element.appendChild(tag);
+}
+
 
 
